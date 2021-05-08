1de3fe88f0d23cc6437a1a6f8843b3e9cf473288
Alex <ashangin@hotmail.com>
2021-05-08 15:08:54 +0300

    Auth

--- Spotify/Controllers/CoreTabs/HomeViewController.swift
+++ Spotify/Controllers/CoreTabs/HomeViewController.swift
@@ -13,7 +13,6 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
         title = "Home"
         view.backgroundColor = .systemBackground
-        print("loaded")
     }
 
 
