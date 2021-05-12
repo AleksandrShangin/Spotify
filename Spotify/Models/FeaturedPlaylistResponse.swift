d0f1ce856141e7f9b7f16c3e8a878d15d771ffb0
Alex <ashangin@hotmail.com>
2021-05-12 12:29:23 +0300

    APICaller II

--- Spotify/Models/FeaturedPlaylistResponse.swift
+++ Spotify/Models/FeaturedPlaylistResponse.swift
@@ -12,18 +12,8 @@ struct FeaturedPlaylistResponse: Codable {
     let playlists: PlaylistResponse
 }
 
-
 struct PlaylistResponse: Codable {
     let items: [Playlist]
-}
-
-struct Playlist: Codable {
-    let description: String
-    let external_urls: [String: String]
-    let id: String
-    let images: [APIImage]
-    let name: String
-    let owner: User
 }
 
 struct User: Codable {
---
+++ Spotify/Models/RecommendationsResponse.swift
@@ -0,0 +1,14 @@
+//
+//  RecommendationsResponse.swift
+//  Spotify
+//
+//  Created by Alex on 5/12/21.
+//
+
+import Foundation
+
+
+struct RecommendationsResponse: Codable {
+    let tracks: [AudioTrack]
+}
+
