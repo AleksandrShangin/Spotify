8effd6785484166ce1f1ef63deae5fd489b5f42a
Alex <ashangin@hotmail.com>
2021-05-14 17:26:54 +0300

    New Releases Cell

---
+++ Spotify/ViewModels/NewReleasesCellViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  NewReleasesCellViewModel.swift
+//  Spotify
+//
+//  Created by Alex on 5/13/21.
+//
+
+import Foundation
+
+
+struct NewReleasesCellViewModel {
+    let name: String
+    let artworkURL: URL?
+    let numberOfTracks: Int
+    let artistName: String
+}
